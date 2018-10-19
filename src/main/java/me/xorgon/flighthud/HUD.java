c2bacd95e572fe53a388f723357bf0ae381294c1
Xorgon <norman.reggox@gmail.com>
2018-10-19 23:11:55 +0100

    Added durability tracking and updated to 1.13.1.

--- src/main/java/me/xorgon/flighthud/HUD.java
+++ src/main/java/me/xorgon/flighthud/HUD.java
@@ -2,7 +2,9 @@ package me.xorgon.flighthud;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.GameMode;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.scoreboard.DisplaySlot;
 import org.bukkit.scoreboard.Objective;
 import org.bukkit.scoreboard.Scoreboard;
@@ -33,19 +35,21 @@ public class HUD {
         obj.getScore(ChatColor.GREEN + "Ground speed").setScore(0);
         obj.getScore(ChatColor.GREEN + "Heading").setScore(0);
         obj.getScore(ChatColor.GREEN + "Sink rate b/s").setScore(0);
+        obj.getScore(ChatColor.GREEN + "Durability %").setScore(0);
 
         player.setScoreboard(scoreboard);
 
         taskID = Bukkit.getScheduler().scheduleSyncRepeatingTask(plugin, new HUDScheduler(this), 0, 2);
     }
 
-    public void setValues(int airspeed, int groundSpeed, int heading, int sinkRate) {
+    public void setValues(int airspeed, int groundSpeed, int heading, int sinkRate, int durability) {
         Objective obj = scoreboard.getObjective(DisplaySlot.SIDEBAR);
         obj.setDisplayName("FlightHUD");
         obj.getScore(ChatColor.GREEN + "Airspeed b/s").setScore(airspeed);
         obj.getScore(ChatColor.GREEN + "Ground speed").setScore(groundSpeed);
         obj.getScore(ChatColor.GREEN + "Heading").setScore(heading);
         obj.getScore(ChatColor.GREEN + "Sink rate b/s").setScore(sinkRate);
+        obj.getScore(ChatColor.GREEN + "Durability %").setScore(durability);
     }
 
     public Player getPlayer() {
@@ -78,13 +82,18 @@ public class HUD {
             Vector v = hud.getPlayer().getVelocity();
 
             int airspeed = (int) Math.round(20 * v.length());
-            int sinkRate = - (int) Math.round(20 * v.getY());
+            int sinkRate = -(int) Math.round(20 * v.getY());
             int groundSpeed = (int) Math.round(20 * Math.sqrt(v.getX() * v.getX() + v.getZ() * v.getZ()));
             int heading = Math.round(player.getLocation().getYaw());
             if (heading < 0) {
                 heading += 360;
             }
-            hud.setValues(airspeed, groundSpeed, heading, sinkRate);
+            ItemStack elytra = player.getInventory().getChestplate();
+            short cur_dur = elytra.getDurability();
+            short max_dur = elytra.getType().getMaxDurability();
+            int durability = Math.round(100 - 100 * cur_dur / max_dur);
+
+            hud.setValues(airspeed, groundSpeed, heading, sinkRate, durability);
         }
     }
 }
